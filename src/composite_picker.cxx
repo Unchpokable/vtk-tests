c261f925593adb97aaeddab6551d15e81512f363
Unchpokable <unchpokable.official@gmail.com>
2025-06-30 19:00:37 +0500

    fixed fucker compilation errors caused renaming

--- src/composite_picker.cxx
+++ src/composite_picker.cxx
@@ -2,15 +2,15 @@
 
 #include "composite_picker.hxx"
 
-scene::CompositeRenderer::id_type scene::pick::perform_pick(const CompositeRenderer& renderer, common::Vec3d screen_pos)
+scene::id_type scene::pick::perform_pick(const CompositeSceneObject& target, common::Vec3d screen_pos)
 {
     vtkNew<vtkCellPicker> picker;
     picker->PickFromListOn();
-    picker->AddPickList(renderer.actor());
+    picker->AddPickList(target.actor());
 
     screen_pos.z() = 0;
 
-    auto picked = picker->Pick(screen_pos.x(), screen_pos.y(), screen_pos.z(), renderer.renderer());
+    auto picked = picker->Pick(screen_pos.x(), screen_pos.y(), screen_pos.z(), target.renderer());
 
     if(picked == 0) {
         return NO_PICK;
@@ -20,9 +20,9 @@ scene::CompositeRenderer::id_type scene::pick::perform_pick(const CompositeRende
 
     auto poly_data = vtkPolyData::SafeDownCast(picked_data);
 
-    if(renderer.is_owns(poly_data)) {
-        auto block_id = renderer.get_block_id(poly_data);
-        return renderer.get_instance_id(block_id);
+    if(target.is_owns(poly_data)) {
+        auto block_id = target.get_block_id(poly_data);
+        return target.get_instance_id(block_id);
     }
 
     return NO_PICK;
--- src/composite_picker.hxx
+++ src/composite_picker.hxx
@@ -1,15 +1,16 @@
-#include "composite_renderer.hxx"
+#include "composite_scene_globals.hxx"
+#include "composite_scene_object.hxx"
 
 namespace scene
 {
 namespace pick
 {
-constexpr CompositeRenderer::id_type NO_PICK = -1;
+constexpr scene::id_type NO_PICK = -1;
 
 /// @brief Performs a pick from target composite renderer and returns picked instance id
-/// @param renderer CompositeRenderer to be a pick target
+/// @param target CompositeObject to be a pick target
 /// @param screen_pos Position of cursor on screen
 /// @return instance id of picked block from target composite renderer or NO_PICK constant if there is no block picked
-CompositeRenderer::id_type perform_pick(const CompositeRenderer& renderer, common::Vec3d screen_pos);
+scene::id_type perform_pick(const CompositeSceneObject& target, common::Vec3d screen_pos);
 } // namespace pick
 } // namespace scene
